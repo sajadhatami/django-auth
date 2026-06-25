dc2cb442e5a805dc823833999a1b8ec4cf91341c
Sajad <sajadhatamiw@gmail.com>
2026-06-25 22:54:31 +0330

    v1: basic session base auth

--- auth/config/settings.py
+++ auth/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
+SESSION_COOKIE_HTTPONLY = True
+#SESSION_COOKIE_SECURE = True 
